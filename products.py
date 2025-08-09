578847457ff5a965c258917322bd1c9b87719784
Jens M. <jensmayer89@gmail.com>
2025-08-09 17:03:34 +0200

    Adding Product class

---
+++ products.py
@@ -0,0 +1,13 @@
+class Product:
+
+    def __init__(self, name, price, quantity):
+        if not name:
+            raise ValueError("Name cannot be empty")
+        if price < 0:
+            raise ValueError("Price cannot be negative")
+        if quantity < 0:
+            raise ValueError("Quantity cannot be negative")
+
+        self.name = name
+        self.price = price
+        self.quantity = quantity
