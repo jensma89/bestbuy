23f3eebaf98ac7140d532af7740d2ce6f3572e00
Jens M. <jensmayer89@gmail.com>
2025-08-10 14:30:44 +0200

    Set a initial stock for products

--- main.py
+++ main.py
@@ -2,15 +2,22 @@ import products
 import store
 
 def main():
-    product_list = [products.Product("MacBook Air M2", price=1450, quantity=100),
-                    products.Product("Bose QuietComfort Earbuds", price=250, quantity=500),
-                    products.Product("Google Pixel 7", price=500, quantity=250),
+
+    # setup initial stock of inventory
+    product_list = [products.Product("MacBook Air M2",
+                                     price=1450,
+                                     quantity=100),
+                    products.Product("Bose QuietComfort Earbuds",
+                                     price=250,
+                                     quantity=500),
+                    products.Product("Google Pixel 7",
+                                     price=500,
+                                     quantity=250)
                     ]
 
-    best_buy = store.Store(product_list)  # auch hier fehlte "store." vor Store
-    active_products = best_buy.get_all_products()
-    print(best_buy.get_total_quantity())
-    print(best_buy.order([(active_products[0], 1), (active_products[1], 2)]))
+
+    best_buy = store.Store(product_list)
+    
 
 
 if __name__ == "__main__":
